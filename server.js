268391b14ca2ae8bb54d535d3f42903c3addebaa
Léo Baecker <leo.baecker@epitech.eu>
2016-03-07 14:12:00 +0100

    CoC Api /api/clan/:clantag

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-var express		= require('express');
-var app			= express();
+"use strict";
+
+var express				= require('express');
+var app					= express();
 var bodyParser 	        = require('body-parser');
 var mongoose 	        = require('mongoose');
 var randtoken			= require('rand-token');
+var clashApi 			= require('clash-of-clans-api');
 // var Bear		= require('./models/bear');
 // var Shack		= require('./models/noelshack');
-var Users		= require('./models/users');
+var Users				= require('./models/users');
 
 // var db = mongoose.connection;
 mongoose.connect('mongodb://127.0.0.1:27017/cocmanager');
@@ -108,6 +111,19 @@ router.route('/signin/:username/:password')
         }
     });
 });
+
+router.route('/clan/:tag')
+	.get(function(req, res) {
+		console.log("Clan tag");
+		let client = clashApi({
+  			token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjcyNmFmMmMzLTg4M2MtNDYwYy1iYTNhLWJiZTk2ZDc5MGMyMCIsImlhdCI6MTQ1Njc4MzA2Niwic3ViIjoiZGV2ZWxvcGVyLzhhODkwMzQzLWU0ZDAtYjlmNS1mNGFjLTljN2FhYTQwNmI1ZCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ2LjIyOS4xNTMuMTQ2Il0sInR5cGUiOiJjbGllbnQifV19._6932djrckwHwIhf0rRSUrwIogC4EI7IdrwDT6K1hro2XNh_ISM2Wib5P8NBGCaNmYQ7iMWtEfktQRTGgSVZbQ" // Optional, can also use COC_API_TOKEN env variable
+		});
+	client
+	  .clanMembersByTag('#82PQ82VV')
+	  .then(response => res.send(response))
+	  .catch(err => console.log(err));
+});
+
 
 router.route('/profile/:profile/:user')
 	.post(function(req, res) {
